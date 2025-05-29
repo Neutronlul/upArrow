cc6b8c8e98280ea8c3f89f6cc07dacab3a7d4ee5
Neutronlul <53470063+Neutronlul@users.noreply.github.com>
2025-05-28 21:04:56 -0700

    Re-enabled std namespace

--- upArrow.cpp
+++ upArrow.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
 
     //dpp::cluster bot(BOT_TOKEN, dpp::i_default_intents | dpp::i_message_content);
     //Test
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << std::endl;
 
     return 0;
 }
