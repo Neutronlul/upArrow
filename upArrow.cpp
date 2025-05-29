8e97cdc59b6c8de7c2f8e59ccb8d786b9472d396
Neutronlul <53470063+Neutronlul@users.noreply.github.com>
2025-05-28 20:59:05 -0700

    Removed std namespace

--- upArrow.cpp
+++ upArrow.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 
-using namespace std;
-
 int main() {
 
     //dpp::cluster bot(BOT_TOKEN, dpp::i_default_intents | dpp::i_message_content);
     //Test
-    cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
 
     return 0;
 }
